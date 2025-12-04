b5f87194c8a450a0aebfa27b994e211f8bca28f0
Fabian Piper <fabianpiper@web.de>
2025-12-04 10:35:07 +0100

    feat: init base logger

---
+++ packages/sdk/src/core/base/logger.ts
@@ -0,0 +1,40 @@
+export type LoggerOptions = {
+  enabled?: boolean;
+  prefix?: string;
+};
+
+export class Logger {
+  private enabled: boolean;
+  private readonly prefix?: string;
+
+  constructor(options?: LoggerOptions) {
+    this.enabled = options?.enabled ?? false;
+    this.prefix = options?.prefix;
+  }
+
+  private format(message: unknown): string {
+    return this.prefix ? `[${this.prefix}] ${message}` : String(message);
+  }
+
+  debug(...args: unknown[]) {
+    if (!this.enabled) return;
+    console.debug(this.format(args[0]), ...args.slice(1));
+  }
+
+  info(...args: unknown[]) {
+    if (!this.enabled) return;
+    console.info(this.format(args[0]), ...args.slice(1));
+  }
+
+  error(...args: unknown[]) {
+    console.error(this.format(args[0]), ...args.slice(1));
+  }
+
+  enable() {
+    this.enabled = true;
+  }
+
+  disable() {
+    this.enabled = false;
+  }
+}
