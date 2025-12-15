b8885e035e2a26bc9614175606401479d2b65d1c
Fabian Piper <fabianpiper@web.de>
2025-12-15 12:29:31 +0100

    feat: init logo

--- apps/web/src/components/layout/Header/index.tsx
+++ apps/web/src/components/layout/Header/index.tsx
@@ -1,19 +1,14 @@
 'use client';
 
+import { useOrderbookData } from '@krono/sdk/react';
+import { cn } from '@ui/lib';
+import Image from 'next/image';
+import Link from 'next/link';
+import type { HTMLAttributes } from 'react';
+import kronoImage from '@/assets/krono.png';
 import MountainIcon from '@/components/icons/MountainIcon';
 import LayoutHeaderSettingsPopover from '@/components/layout/Header/SettingsPopover';
 import { OrderbookPanelSelect } from '@/components/orderbook/Panel/Symbol';
-import {
-  useOrderbookConfig,
-  useOrderbookData,
-  useOrderbookStatus,
-} from '@krono/sdk/react';
-import { Button } from '@ui/components/ui/button';
-import { Label } from '@ui/components/ui/label';
-import { Switch } from '@ui/components/ui/switch';
-import { cn } from '@ui/lib';
-import { format } from 'date-fns';
-import type { HTMLAttributes } from 'react';
 
 export type LayoutHeaderProps = HTMLAttributes<HTMLDivElement>;
 
@@ -32,8 +27,14 @@ export default function LayoutHeader({
       )}
       {...props}
     >
-      <div className="flex items-center gap-2">
-        <MountainIcon className="h-6 w-6" />
+      <div className="flex items-center gap-4">
+        <Image
+          src={kronoImage}
+          alt={'Krono'}
+          className="w-8 h-8 rounded-lg"
+          width={40}
+          height={40}
+        />
         <span className="sr-only">Krono</span>
         <OrderbookPanelSelect />
         {/*
@@ -42,7 +43,23 @@ export default function LayoutHeader({
         </div>
 */}
       </div>
-      <div className={'flex justify-end items-center gap-2'}>
+      <div className={'flex justify-end items-center gap-6'}>
+        <Link
+          href={'/about'}
+          className={
+            'text-sm text-foreground/60 transition-color ease-in-out duration-200 font-medium hover:text-foreground visited:text-foreground'
+          }
+        >
+          About
+        </Link>
+        <Link
+          href={'/docs'}
+          className={
+            'text-sm text-foreground/60 transition-color ease-in-out duration-200 font-medium hover:text-foreground visited:text-foreground'
+          }
+        >
+          Documentation
+        </Link>
         <LayoutHeaderSettingsPopover />
       </div>
 
