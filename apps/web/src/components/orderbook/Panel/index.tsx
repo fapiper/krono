90184a3cc2b4bb1075068a9b132610ee333767ba
Fabian Piper <fabianpiper@web.de>
2025-12-19 11:47:51 +0100

    fix web styling

--- apps/web/src/components/orderbook/Panel/index.tsx
+++ apps/web/src/components/orderbook/Panel/index.tsx
@@ -5,8 +5,8 @@ import { Card, CardContent } from '@ui/components/ui/card';
 
 export function OrderbookPanel() {
   return (
-    <Card className={'relative flex flex-1 flex-col group'}>
-      <CardContent className="grid grid-cols-1 md:grid-cols-2 pb-px pt-2 text-xs gap-0.5 flex-1">
+    <Card className={'flex flex-1'}>
+      <CardContent className="flex flex-1 px-0 pb-px pt-2 overflow-hidden">
         <OrderbookTable.Root />
       </CardContent>
     </Card>
